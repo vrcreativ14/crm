b2ba0f1e37cfabaf0752e317e6c4c9171f14fcc4
Kartikey Mishra <vrcreative14@gmail.com>
2022-02-21 06:33:25 +0000

    Update settings.py

--- felix/settings.py
+++ felix/settings.py
@@ -350,6 +350,7 @@ _1mb = 1024 * 1024
 LOG_DIR = env('LOG_DIR')
 CSRF_TRUSTED_ORIGINS = ['https://crm1.insurenex.io']
 CORS_ORIGIN_WHITELIST = ('https://crm1.insurenex.io',)
+CSRF_COOKIE_DOMAIN = ".crm1.insurenex.io"
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
