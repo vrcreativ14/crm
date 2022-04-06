fbcc11065e49cdd7a9097dda3b63510357860b5f
vrcreative14 <69114615+vrcreative14@users.noreply.github.com>
2022-04-06 13:24:53 +0530

    agent id in payload for whatsapp message from the motor app

--- felix/message.py
+++ felix/message.py
@@ -9,12 +9,12 @@ class WhatsappService:
     def send_whatsapp_msg(self, to, message, **kwargs):
         app_name = kwargs.get('app_name',None)
         token = ''
+        payload = {}
+        
         if app_name == 'mortgage':
             token = 'ef54c9d79ec8fc5b0da514ce04e7dd617783965e27271dca47725995fe59c8a017b1a50246a8b227'
-        else:
-            token = '6479e9c54e58b868088a3c8219bf5aeb114b0d4abf48911487ecbd5a69a38eda9ecb3f6290116a1a'
-        payload = {
-            "phone": to,
+            payload = {
+            "phone": to,             
             "message": message,
             "actions": [
                     {
@@ -22,6 +22,20 @@ class WhatsappService:
                     }
                 ]
             }
+        else:
+            token = '6479e9c54e58b868088a3c8219bf5aeb114b0d4abf48911487ecbd5a69a38eda9ecb3f6290116a1a'
+            motor_agent_id = '62382cb386070b68c50f4c93'
+            payload = {
+            "phone": to,
+            "agent": motor_agent_id,
+            "message": message,
+            "actions": [
+                    {
+                      "action": "chat:resolve"
+                    }
+                ]
+            }
+        
         headers = {
             "Content-Type": "application/json",
             "Token": token
