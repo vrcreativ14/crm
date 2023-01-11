b19ff7312329dd4f5c5770dabb238358dfa1377b
shubhangi mishra <vrcreative14@gmail.com>
2023-01-11 18:35:55 +0530

    bcc_email to ind.mdeical added

--- healthinsurance/tasks.py
+++ healthinsurance/tasks.py
@@ -5,8 +5,8 @@ from celery import shared_task
 import time 
 
 @app.task
-def email_notification(deal, email_type, recipient, cc_emails = []):
+def email_notification(deal, email_type, recipient, cc_emails = [], bcc_emails = []):
     logger = logging.getLogger('workers')
     logger.info('Sending an email notification to customer')
-    stage_email = StageEmailNotification(deal, email_type, recipient, cc_addresses = cc_emails)
+    stage_email = StageEmailNotification(deal, email_type, recipient, cc_addresses = cc_emails, bcc_addresses=bcc_emails)
     stage_email.stage_propagation_email()
